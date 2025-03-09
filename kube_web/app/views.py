cde92296c0a5d670416aee644cbcc92799da9844
MarioBernaldez <maariobernaldez@uma.es>
2025-03-09 20:23:45 +0100

    feat: Add dynamic functionality to add and remove containers, volumes, and volume mounts

--- kube_web/app/views.py
+++ kube_web/app/views.py
@@ -1,37 +1,43 @@
+from django.forms import formset_factory
 from django.shortcuts import render
 from django.http import JsonResponse
 from .forms import DeploymentForm, PodTemplateForm, ContainerForm, VolumeMountForm, VolumeForm
 
 def deployment_config_view(request):
+    ContainerFormSet = formset_factory(ContainerForm, extra=1)
+    VolumeFormSet = formset_factory(VolumeForm, extra=1)
+    VolumeMountFormSet = formset_factory(VolumeMountForm, extra=1)
+
     if request.method == "POST":
         deployment_form = DeploymentForm(request.POST)
         pod_form = PodTemplateForm(request.POST)
-        container_form = ContainerForm(request.POST)
-        volume_mount_form = VolumeMountForm(request.POST)
-        volume_form = VolumeForm(request.POST)
+        container_formset = ContainerFormSet(request.POST, prefix="containers")
+        volume_formset = VolumeFormSet(request.POST, prefix="volumes")
+        volume_mount_formset = VolumeMountFormSet(request.POST, prefix="volume_mounts")
 
-        if all([deployment_form.is_valid(), pod_form.is_valid(), container_form.is_valid(), volume_mount_form.is_valid(), volume_form.is_valid()]):
-            # Generación del JSON con los datos tal cual fueron ingresados
+        if (deployment_form.is_valid() and pod_form.is_valid() and 
+            container_formset.is_valid() and volume_formset.is_valid() and 
+            volume_mount_formset.is_valid()):
+            # Genera el JSON con los datos ingresados
             user_input_data = {
                 "deployment": deployment_form.cleaned_data,
                 "pod_template": pod_form.cleaned_data,
-                "container": container_form.cleaned_data,
-                "volume_mount": volume_mount_form.cleaned_data,
-                "volume": volume_form.cleaned_data
+                "containers": [form.cleaned_data for form in container_formset],
+                "volumes": [form.cleaned_data for form in volume_formset],
+                "volume_mounts": [form.cleaned_data for form in volume_mount_formset]
             }
             return JsonResponse(user_input_data, json_dumps_params={'indent': 4})
-
     else:
         deployment_form = DeploymentForm()
         pod_form = PodTemplateForm()
-        container_form = ContainerForm()
-        volume_mount_form = VolumeMountForm()
-        volume_form = VolumeForm()
+        container_formset = ContainerFormSet(prefix="containers")
+        volume_formset = VolumeFormSet(prefix="volumes")
+        volume_mount_formset = VolumeMountFormSet(prefix="volume_mounts")
 
     return render(request, "deployment_form.html", {
         "deployment_form": deployment_form,
         "pod_form": pod_form,
-        "container_form": container_form,
-        "volume_mount_form": volume_mount_form,
-        "volume_form": volume_form
+        "container_formset": container_formset,
+        "volume_formset": volume_formset,
+        "volume_mount_formset": volume_mount_formset
     })
